0d81398da92cd4b0ada735154c8013a37f212edf
Shih Han <swang05@mail.bbk.ac.uk>
2018-04-17 15:42:44 +0100

    reset temperature

--- spec/FeatureSpec.js
+++ spec/FeatureSpec.js
@@ -1,63 +1,3 @@
-// describe('Airport', function() {
-//
-//   var airport;
-//
-//   beforeEach(function() {
-//    // airport = new Airport();
-//    plane = new Plane();
-//    });
-//
-//    describe('has the plane after landing', function() {
-//       it('has the plane', function() {
-//           var airport1 = new Airport()
-//           airport1.land(plane, 'sunny')
-//        expect(airport1.hangar).toContain(plane);
-//      });
-//    });
-//
-//    describe('does not hava the plane after taking off', function() {
-//      it('has no plane', function() {
-//         var airport2 = new Airport()
-//         airport2.take_off(plane, 'sunny')
-//        expect(airport2.hangar).not.toContain(plane);
-//      });
-//    });
-//
-//    describe('when stormy, plane is not allowed to', function() {
-//      it('land', function() {
-//       var airport3 = function(){
-//         airport3 = new Airport()
-//         airport3.land(plane, 'stormy')
-//       }
-//        expect(airport3).toThrowError('Plane is not allowed to land when stormy');
-//      });
-//
-//      it('take off', function() {
-//       var airport4 = function(){
-//         airport4 = new Airport()
-//         airport4.take_off(plane, 'stormy')
-//       }
-//        expect(airport4).toThrowError('Plane is not allowed to take off when stormy');
-//      });
-//    });
-//
-//    describe('raises an error when', function() {
-//      it('airport is full', function() {
-//        var airport5 = function(){
-//         airport5 = new Airport(3)
-//         var times = 4;
-//         for(var i=0; i < times; i++){
-//           airport5.land(plane, 'sunny');
-//         }
-//        }
-//        expect(airport5).toThrowError('Airport is full');
-//      });
-//    });
-//
-//
-//
-//   });
-
 describe('Thermostat', function() {
 
      describe('starts at', function() {
@@ -114,10 +54,17 @@ describe('Thermostat', function() {
         });
 
      });
-     //        var airport5 = function(){
-     //         airport5 = new Airport(3)
-     //         var times = 4;
-     //         for(var i=0; i < times; i++){
-     //           airport5.land(plane, 'sunny');
-     //         }
+
+     describe('reset', function() {
+        it('degree is 20', function() {
+          thermostat7 = new Thermostat();
+          var times = 5;
+          for(var i=0; i < times; i++){
+            thermostat5.up()
+          };
+          thermostat7.reset
+         expect(thermostat7.degree).toEqual(20);
+        });
+     });
+
 });
--- src/thermostat.js
+++ src/thermostat.js
@@ -24,48 +24,6 @@ Thermostat.prototype.down = function(){
   }
 };
 
-
-
-// Array.prototype.random = function(){
-//   return this[Math.floor(Math.random() * this.length)]
-// };
-//
-// var DEFAULT_CAPACITY = function(){
-//   return 50
-// };
-//
-// var Plane = function (){};
-//
-// var Airport = function (capacity = DEFAULT_CAPACITY){
-//   this.weather = ["stormy", "sunny", "sunny", "sunny"].random();
-//   this.hangar = []
-//   this.capacity = capacity
-// };
-//
-// // Airport.prototype._isfull = function(length = this.hangar.length, capacity = this.capacity){
-// //   if(length === capacity){
-// //     return ture;
-// //   }
-// //    return false;
-// // };
-//
-// Airport.prototype.land = function(plane, weather = this.weather) {
-//   if (weather === 'stormy'){
-//     throw new TypeError("Plane is not allowed to land when stormy")
-//   }
-//   if (this.hangar.length === this.capacity ){
-//     throw new TypeError('Airport is full')
-//   }
-//   else{
-//     this.hangar.push(plane)
-//   }
-// };
-//
-// Airport.prototype.take_off = function(plane, weather = this.weather) {
-//   if (weather === 'stormy'){
-//     throw new TypeError('Plane is not allowed to take off when stormy')
-//   }
-//   else{
-//     this.hangar.pop(plane)
-//   }
-// };
+Thermostat.prototype.reset = function(){
+  this.degree = 20;
+};
