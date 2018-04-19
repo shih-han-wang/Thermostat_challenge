89d31a646f35e0211c01c5c35b5c4c4325c3a464
Shih Han <swang05@mail.bbk.ac.uk>
2018-04-19 16:36:45 +0100

    current weather display by city selected

--- src/interface.js
+++ src/interface.js
@@ -1,7 +1,9 @@
 $(document).ready(function(){
   var thermostat = new Thermostat();
+
   degreeUpdate()
   savingModeUpdate()
+  displayCity('london')
 
   $("#degree-up").click(function(){
       thermostat.up();
@@ -28,6 +30,11 @@ $(document).ready(function(){
       savingModeUpdate()
   });
 
+  $('#current-city').change(function(){
+    var city = $('#current-city').val();
+    displayCity(city)
+  });
+
   function degreeUpdate(){
     $("#degree").text(thermostat.currentDegree())
 
@@ -44,4 +51,12 @@ $(document).ready(function(){
       $("#prower-saving-mode").text(thermostat.powerSavingMode())
   };
 
+  function displayCity(city){
+    var url = 'http://api.openweathermap.org/data/2.5/weather?q=' + city;
+    var appid = '&appid=4c97c369ec893101e67de19569afb141';
+    var units = '&units=metric';
+    $.get(url + appid + units, function(data) {
+      $('#current-temperature').text(data.main.temp);
+    });
+  };
 });
